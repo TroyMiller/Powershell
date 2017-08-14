fc71f94f9c0608993942c2f3ccf495918ea6c842
TroyMiller <troy.miller@gmail.com>
2017-08-14 15:31:00 -0500

    Added vHosts and vDatastore
    Added imports for vHost and vDatastore.
    Added tables to sql build.

--- rvtools/import_rvtools_csv.ps1
+++ rvtools/import_rvtools_csv.ps1
@@ -22,6 +22,7 @@ Function ExportWSToCSV ($excelFile, $csvLoc)
     Remove-Variable E
 }
 
+
 function Get-FileName($initialDirectory)
 {
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
@@ -85,7 +86,7 @@ function Import-vInfo($sqlserver, $database, $sqluser, $sqlpassword)
     
     $impcsv = invoke-sqlcmd -Database $db_name -Query $query  -serverinstance $sql_instance_name -verbose @auth
     
-    write-host "vHost Processing row ..........$count" -foregroundcolor green 
+    write-host "vInfo Processing row ..........$count" -foregroundcolor green 
     
     $count  = $count + 1 
     
@@ -270,6 +271,127 @@ function Import-vCluster($sqlserver, $database, $sqluser, $sqlpassword)
     } 
 }
 
+
+
+function Import-vHost($sqlserver, $database, $sqluser, $sqlpassword)
+{
+
+    
+    $auth=@{UserName=$sqluser;Password=$sqlpassword}
+    $sql_instance_name = $sqlserver 
+    $db_name = $database
+    
+    $impcsv = (get-childitem C:\CSVFiles\*vhost*.csv).FullName
+    $data = import-csv $impcsv 
+    $count = 1 
+    
+    foreach($i in $data){ 
+        $scandate = $date
+        $customer = $customer
+        $HostName = $i.host
+        $Datacenter = $i.datacenter
+        $CPU_Model = $i."cpu model"
+        $Speed = $i.speed.replace(",","")
+        $HT_Available = $i."HT Available"
+        $HT_Active = $i."HT Active"
+        $num_CPU = $i."# CPU"
+        $Cores_per_CPU = $i."Cores per CPU"
+        $num_Cores = $i."# Cores"
+        $CPU_usage = $i."CPU usage%"
+        $num_Memory = $i."# Memory".replace(",","")
+        $Memory_usage = $i."Memory usage %"
+        $num_NICs = $i."# NICs"
+        $num_HBAs = $i."# HBAs"
+        $num_VMs = $i."# VMs"
+        $VMs_per_Core = $i."VMs per Core"
+        $num_vCPUs = $i."# vCPUs"
+        $vCPUs_per_Core = $i."vCPUs per Core"
+        $vRAM = $i.vRAM.replace(",","")
+        $VM_Used_memory = $i."VM Used memory".replace(",","")
+        $VMotion_support = $i."VMotion support"
+        $Storage_VMotion_support = $i."Storage VMotion support"
+        $Current_EVC = $i."Current EVC"
+        $Max_EVC = $i."Max EVC"
+        $ESX_Version = $i."ESX Version"
+        $Domain = $i.Domain	
+        $DNS_Search_Order = $i."DNS Search Order"
+        $NTP_Servers = $i."NTP Server(s)"
+        $Vendor = $i.Vendor
+        $Model = $i.Model
+        $Service_tag = $i."Service tag"
+        $OEM_specific_string = $i."OEM specific string"
+        $BIOS_Version = $i."BIOS Version"
+        $BIOS_Date = $i."BIOS Date"
+        $Object_ID = $i."Object ID"
+        $VI_SDK_Server = $i."vi sdk server"
+        $VI_SDK_UUID = $i."vi sdk uuid"
+       
+    
+    $query = "INSERT INTO vHost (scandate, customer, Hostname, Datacenter, CPU_Model, Speed, HT_Available, HT_Active, num_CPU, Cores_per_CPU, num_Cores, CPU_usage, num_Memory, Memory_usage, num_NICs, num_HBAs, num_VMs, VMs_per_Core, num_vCPUs, vCPUs_per_Core, vRAM, VM_Used_memory, VMotion_support, Storage_VMotion_support, Current_EVC, Max_EVC, ESX_Version, Domain, DNS_Search_Order, NTP_Servers, Vendor, Model, Service_tag, OEM_specific_string, BIOS_Version, BIOS_Date, Object_ID, VI_SDK_Server, VI_SDK_UUID) 
+                VALUES ('$scandate','$customer','$Hostname','$Datacenter','$CPU_Model','$Speed','$HT_Available','$HT_Active','$num_CPU','$Cores_per_CPU','$num_Cores','$CPU_usage','$num_Memory','$Memory_usage','$num_NICs','$num_HBAs','$num_VMs','$VMs_per_Core','$num_vCPUs','$vCPUs_per_Core','$vRAM','$VM_Used_memory','$VMotion_support','$Storage_VMotion_support','$Current_EVC','$Max_EVC','$ESX_Version','$Domain','$DNS_Search_Order','$NTP_Servers','$Vendor','$Model','$Service_tag','$OEM_specific_string','$BIOS_Version','$BIOS_Date','$Object_ID','$VI_SDK_Server','$VI_SDK_UUID')" 
+    
+    $impcsv = invoke-sqlcmd -Database $db_name -Query $query  -serverinstance $sql_instance_name -verbose @auth
+    
+    write-host "vHost Processing row ..........$count" -foregroundcolor green 
+    
+    $count  = $count + 1 
+    
+    } 
+}
+
+
+function Import-vDatastore($sqlserver, $database, $sqluser, $sqlpassword)
+{
+
+    
+    $auth=@{UserName=$sqluser;Password=$sqlpassword}
+    $sql_instance_name = $sqlserver 
+    $db_name = $database
+    
+    $impcsv = (get-childitem C:\CSVFiles\*vdatastore*.csv).FullName
+    $data = import-csv $impcsv 
+    $count = 1 
+    
+    foreach($i in $data){ 
+        $scandate = $date
+        $customer = $customer
+        $Name = $i.Name
+        $Address = $i.Address
+        $Accessible = $i.Accessible
+        $Type = $i.Type
+        $num_VMs = $i."# VMs"
+        $Capacity_MB = $i."Capacity MB".replace(",","")
+        $Provisioned_MB = $i."Provisioned MB".replace(",","")
+        $In_Use_MB = $i."In Use MB".replace(",","")
+        $Free_MB_Free = $i."Free MB Free %"
+        $SIOC_enabled = $i."SIOC enabled"
+        $SIOC_Threshold = $i."SIOC Threshold"
+        $num_Hosts = $i."# Hosts"
+        $Hosts = $i.Hosts
+        $Block_size = $i."Block size".replace(",","")
+        $Max_Blocks = $i."Max Blocks"
+        $num_Extents = $i."# Extents"
+        $Major_Version = $i."Major Version"
+        $Version = $i.Version
+        $VMFS_Upgradeable = $i."VMFS_Upgradeable"
+        $MHA = $i.MHA
+        $URL = $i.URL
+        $VI_SDK_Server = $i."vi sdk server"
+        $VI_SDK_UUID = $i."vi sdk uuid"
+       
+    
+    $query = "INSERT INTO vDatastore (scandate, customer, dsName, dsAddress, Accessible, dsType, num_VMs, Capacity_MB, Provisioned_MB, In_Use_MB, Free_MB_Free, SIOC_enabled, SIOC_Threshold, num_Hosts, Hosts, Block_size, Max_Blocks, num_Extents, Major_Version, Version, VMFS_Upgradeable, MHA, dsURL, VI_SDK_Server, VI_SDK_UUID) 
+                VALUES ('$scandate','$customer','$Name','$Address','$Accessible','$Type','$num_VMs','$Capacity_MB','$Provisioned_MB','$In_Use_MB','$Free_MB_Free','$SIOC_enabled','$SIOC_Threshold','$num_Hosts','$Hosts','$Block_size','$Max_Blocks','$num_Extents','$Major_Version','$Version','$VMFS_Upgradeable','$MHA','$URL','$VI_SDK_Server','$VI_SDK_UUID')" 
+    
+    $impcsv = invoke-sqlcmd -Database $db_name -Query $query  -serverinstance $sql_instance_name -verbose @auth
+    
+    write-host "vDatastore Processing row ..........$count" -foregroundcolor green 
+    
+    $count  = $count + 1 
+    
+    } 
+}
+
 $excelpath = Get-FileName "c:\"
 
 #Prompt for Customer Name
@@ -288,6 +410,10 @@ Import-vDisk $sqlserver $database $sqluser $sqlpassword
 Import-vPartition $sqlserver $database $sqluser $sqlpassword
 Import-vHealth $sqlserver $database $sqluser $sqlpassword
 Import-vCluster $sqlserver $database $sqluser $sqlpassword
+Import-vHost $sqlserver $database $sqluser $sqlpassword
 
 #Remove CSV Files
 Remove-item C:\CSVFiles\*.csv -Confirm:$false
+
+#Final Notice
+Write-Verbose "Successfully imported $excelpath from $date for $customer" -Verbose
